3814e7b8943b83680157503e5eb5341b825f4790
chandra <sekharc446@gmail.com>
2022-05-06 16:47:41 +0530

    added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var gen=document.getElementsByClassName("gen")[0];
 var output=document.getElementsByClassName("output")[0];
-var duration=8000;
+var duration=7000;
 var arr=[2,3,4,5,6,7,9,10,11,12,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,37,38,39,40,41,42,43,44,46,47,48,49,50,51,52,53,54,55,56,57,"le2","le3","le4","le5","le6","le7","le8","le9","le11","le12","le13","le14","17-504"];
 var aud = document.getElementById("myAudio")
 
