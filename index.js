18a6b944d2a8c2532d99c3fadbaf9f2bf3c02728
chandra <sekharc446@gmail.com>
2022-05-06 16:34:36 +0530

    added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var gen=document.getElementsByClassName("gen")[0];
 var output=document.getElementsByClassName("output")[0];
-var duration=10000;
+var duration=8000;
 var arr=[2,3,4,5,6,7,9,10,11,12,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,37,38,39,40,41,42,43,44,46,47,48,49,50,51,52,53,54,55,56,57,"le2","le3","le4","le5","le6","le7","le8","le9","le11","le12","le13","le14","17-504"];
 var aud = document.getElementById("myAudio")
 
@@ -11,7 +11,7 @@ gen.addEventListener("click",function()
     aud.play();
     gen.style.display="none";
     var started=new Date().getTime();
-    if(output.innerHTML!="1000")
+    if(output.innerHTML!="Welcome")
     {
         arr=arr.filter(function(ele)
         {
@@ -39,7 +39,7 @@ gen.addEventListener("click",function()
                 var num=arr[Math.floor(Math.random()*arr.length)];
                 if(num==undefined)
                 {
-                    output.innerHTML="completed";
+                    output.innerHTML="Sayonara";
 
                 }
                 else{
